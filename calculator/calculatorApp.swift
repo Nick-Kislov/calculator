6757fc873f8d097d26e7d7d16c60a9ce1c6b81da
nikolaykislov <nikoalykislov888@gmail.com>
2024-08-09 17:47:12 +0300

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Nikolay Kislov on 09.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
